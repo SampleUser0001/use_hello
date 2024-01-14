b276cd380c9e7daa8a5d034d7ae1645d8d69695c
SampleUser0001 <ittimfn+github@gmail.com>
2024-01-14 23:02:17 +0900

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module use_hello
+
+go 1.21.5
+
+require github.com/SampleUser0001/hello v0.0.0-20240114130042-d026dfab2123
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/SampleUser0001/hello"
+)
+
+func main() {
+	println(hello.Hello("ittimfn"))
+}
